38b5ade52e1bcb9e9d0de45b402a55b7aaece1f1
acwong00 <acwong00@gmail.com>
2015-03-27 13:37:05 +0800

    judge whether the file object exists

--- cleanTorrent.js
+++ cleanTorrent.js
@@ -2,12 +2,11 @@ var fs = require("fs"),
     parseTorrent = require('parse-torrent'),
     md5 = require('MD5');
 
-function main (argv) {
+function main(argv) {
     var cleanInfos = [],
         dir = "./.new";
     for (var i = 0; i < argv.length; i++) {
         info = parseTorrent(fs.readFileSync(argv[i])).info;
-        console.log(info);
         cleanInfos.push(cleanInfo(info));
     }
 
@@ -23,18 +22,20 @@ function main (argv) {
     };
 }
 
-function cleanInfo (info) {
+function cleanInfo(info) {
     info.name = md5(info.name);
     info['name.utf-8'] = md5(info['name.utf-8']);
     var files = info.files;
-    for (var i = 0; i < files.length; i++) {
-        var file = files[i];
-        for (var key in file) {
-            if (key == "path" || key == "path.utf-8") {
-                for (var j = 0; j < file[key].length; j++) {
-                    var text = file[key][j].toString();
-                    var dotIndex = text.lastIndexOf(".");
-                    file[key][j] = md5(text.slice(0,dotIndex)) + text.slice(dotIndex,text.length);
+    if (files) {
+        for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+            for (var key in file) {
+                if (key == "path" || key == "path.utf-8") {
+                    for (var j = 0; j < file[key].length; j++) {
+                        var text = file[key][j].toString();
+                        var dotIndex = text.lastIndexOf(".");
+                        file[key][j] = md5(text.slice(0, dotIndex)) + text.slice(dotIndex, text.length);
+                    }
                 }
             }
         }
