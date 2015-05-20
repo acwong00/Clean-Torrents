3c22d9931b058f34e7751c0fffebde271e63f242
acwong00 <acwong00@gmail.com>
2015-05-20 12:49:37 +0800

    change to nt lib

--- cleanTorrent.js
+++ cleanTorrent.js
@@ -1,46 +1,25 @@
-var fs = require("fs"),
-    parseTorrent = require('parse-torrent'),
-    md5 = require('MD5');
+var path = require('path'),
+    fs = require('fs');
+    nt = require('nt'),
+    md5 = require('MD5'),
+    Metadata = require('./lib/Metadata.js');
 
-function main(argv) {
-    var cleanInfos = [],
-        dir = "./.new";
-    for (var i = 0; i < argv.length; i++) {
-        info = parseTorrent(fs.readFileSync(argv[i])).info;
-        cleanInfos.push(cleanInfo(info));
-    }
-
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
-    }
-
-    for (var i = 0; i < cleanInfos.length; i++) {
-        var buf = parseTorrent.toTorrentFile({
-            info: cleanInfos[i]
-        });
-        var a = fs.writeFile(dir + "/" + cleanInfos[i].name + ".torrent", buf);
-    };
-}
-
-function cleanInfo(info) {
-    info.name = md5(info.name);
-    info['name.utf-8'] = md5(info['name.utf-8']);
-    var files = info.files;
-    if (files) {
-        for (var i = 0; i < files.length; i++) {
-            var file = files[i];
-            for (var key in file) {
-                if (key == "path" || key == "path.utf-8") {
-                    for (var j = 0; j < file[key].length; j++) {
-                        var text = file[key][j].toString();
-                        var dotIndex = text.lastIndexOf(".");
-                        file[key][j] = md5(text.slice(0, dotIndex)) + text.slice(dotIndex, text.length);
-                    }
-                }
-            }
+function main (argv) {
+    argv.forEach(function (item) {
+        var dir = path.resolve(path.dirname(item), "New");
+        if (!fs.existsSync(dir)) {
+            fs.mkdir(dir);
         }
-    }
-    return info;
+        nt.read(item, function (err, torrent) {
+            if (err) {
+                console.log(item + '无效');
+                return;
+            }
+            var metadata = new Metadata(torrent.metadata);
+            torrent.metadata = metadata.cleanMetadata();
+            torrent.createWriteStream(path.resolve(dir, metadata.getName() + ".torrent"));
+        });
+    });
 }
 
 main(process.argv.slice(2));
